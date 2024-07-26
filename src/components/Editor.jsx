38100b9243d80714cc5f419e674809f18d08ebb5
Josue Igiraneza <igiranezaj28@gmail.com>
2024-07-26 15:58:34 +0200

    React scrimba course - Notes app

---
+++ src/components/Editor.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+import ReactMde from "react-mde";
+import "react-mde/lib/styles/css/react-mde-all.css";
+import Showdown from "showdown";
+
+export default function Editor({ currentNote, updateNote }) {
+   const [selectedTab, setSelectedTab] = React.useState("write");
+
+   const converter = new Showdown.Converter({
+      tables: true,
+      simplifiedAutoLink: true,
+      strikethrough: true,
+      tasklists: true,
+   });
+
+   return (
+      <section className="editor pane">
+         <ReactMde
+            value={currentNote.body}
+            onChange={updateNote}
+            selectedTab={selectedTab}
+            onTabChange={setSelectedTab}
+            generateMarkdownPreview={(markdown) =>
+               Promise.resolve(converter.makeHtml(markdown))
+            }
+            minEditorHeight={80}
+            heightUnits="vh"
+         />
+      </section>
+   );
+}
